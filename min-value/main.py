3ffbcd2526b4b1110daad128cc441cf06b4b83fa
mfcruzm-arch <mfcruzm@gmail.com>
2026-09-09 13:04:11 +0200

    hola

---
+++ min-value/main.py
@@ -0,0 +1,15 @@
+def run(values: list) -> int:
+    min_value = values[0]
+
+    for value in values[1:]:
+        if value < min_value:
+            min_value = value
+
+    return min_value
+
+
+# DO NOT TOUCH THE CODE BELOW
+if __name__ == '__main__':
+    import vendor
+
+    vendor.launch(run)
