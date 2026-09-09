67583b5e712f4bc4c45da0ea7f46f6aa99f1f4f9
mfcruzm-arch <mfcruzm@gmail.com>
2026-09-09 11:46:26 +0200

    nuevo

---
+++ dot-product/main.py
@@ -0,0 +1,12 @@
+def run(u: list, v: list) -> float | None:
+    if len(u) != len(v):
+        return None
+
+    return sum(x * y for x, y in zip(u, v))
+
+
+# DO NOT TOUCH THE CODE BELOW
+if __name__ == '__main__':
+    import vendor
+
+    vendor.launch(run)
